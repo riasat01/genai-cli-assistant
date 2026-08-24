4fd4eb7347c7eb1d00917a2049a312305e846fac
riasat01 <riasat01@outlook.com>
2026-08-24 23:53:10 +0600

    system behavior added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from google import genai
 from history_manager import load_history, save_history
+from google.genai import types
 
 load_dotenv()
 
@@ -19,8 +20,20 @@ client = genai.Client()
 
 
 def main():
+    my_system_instruction = (
+        "You are a helpful, witty, and slightly sarcastic coding assistant "
+        "who loves to give funny and easily understandable analogies to explain question or concepts and keeps answers concise."
+    )
+
     saved_history = load_history()
-    chat = client.chats.create(model="gemini-3.6-flash", history=saved_history)
+    chat = client.chats.create(
+        model="gemini-3.6-flash", 
+        history=saved_history,
+        config=types.GenerateContentConfig(
+            system_instruction=my_system_instruction,
+            temperature=0.7
+        )
+    )
     print("🤖 Gemini CLI Assistant Ready! (Type 'exit' or 'quit' to end)\n")
 
     if saved_history:
